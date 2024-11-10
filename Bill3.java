7d0205113896c095ed574da64c2be21971eafcc5
galigoldman <gali.goldman@post.runi.ac.il>
2024-11-10 10:22:33 +0200

    changing the names order

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
     double finalEach=0;
     totalBill = Integer.parseInt(args[3]);
     finalEach = Math.ceil(totalBill/3.0); 
-    System.out.println("Dear " + name1 + ", " + name2 + " and " +
-	name3 + ": pay " + finalEach + " Shekels each.");
+    System.out.println("Dear " + name3 + ", " + name2 + " and " +
+	name1 + ": pay " + finalEach + " Shekels each.");
 	}
 }
