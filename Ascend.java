5aa79a3133b821d9b7fa9837d5278b7a5471e4a6
galigoldman <gali.goldman@post.runi.ac.il>
2024-11-10 10:02:40 +0200

    1st homework, by Gali Goldman

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,16 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int range=0,a=0,b=0,c=0;
+        int max=0, mid=0, min=0;
+        range=Integer.parseInt(args[0]);
+        a = (int)(Math.random() * (range + 1));
+        b = (int)(Math.random() * (range + 1));
+        c = (int)(Math.random() * (range + 1));
+        System.out.println(a+ " "+ b + " " + c); 
+        max = Math.max(a, Math.max(b,c));
+        min = Math.min(a, Math.min(b,c));
+        mid = a + b + c - max - min;
+        System.out.println(min + " "+ mid + " " + max);
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,13 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	// To get you started, here is the first line in the program:
+	String name1 = args[0],name2 = args[1],name3 = args[2];
+    int totalBill=0;
+    double finalEach=0;
+    totalBill = Integer.parseInt(args[3]);
+    finalEach = Math.ceil(totalBill/3.0); 
+    System.out.println("Dear " + name1 + ", " + name2 + " and " +
+	name3 + ": pay " + finalEach + " Shekels each.");
 	}
 }
